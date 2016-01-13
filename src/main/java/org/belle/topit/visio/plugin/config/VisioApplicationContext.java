c98799472c08425320d0682c65e8871fffdf2d60
IvanZhang2013 <gongyishenhuxi2005@163.com>
2016-01-13 17:04:29 +0800

    全文

---
+++ src/main/java/org/belle/topit/visio/plugin/config/VisioApplicationContext.java
@@ -0,0 +1,17 @@
+package org.belle.topit.visio.plugin.config;
+
+import org.belle.topit.visio.plugin.orgmodel.DocumentConfig;
+
+public class VisioApplicationContext {
+	private DocumentConfig documentConfig;
+
+	
+	public DocumentConfig getDocumentConfig() {
+		return documentConfig;
+	}
+
+	public void setDocumentConfig(DocumentConfig documentConfig) {
+		this.documentConfig = documentConfig;
+	}
+
+}
